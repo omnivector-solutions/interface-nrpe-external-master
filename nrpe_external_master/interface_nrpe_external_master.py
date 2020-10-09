926f5ebe550461544c38c5afd428ebe850906d59
jamesbeedy <jamesbeedy@gmail.com>
2020-10-09 00:40:48 +0000

    refactor slurm checks into interface

--- nrpe_external_master/interface_nrpe_external_master.py
+++ nrpe_external_master/interface_nrpe_external_master.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Nrpe external master interface."""
-from datetime import datetime
+import json
 import logging
+import subprocess
+from datetime import datetime
 from pathlib import Path
 
 from ops.framework import (
@@ -51,7 +53,8 @@ class Nrpe(Object):
 
     def _on_relation_changed(self, event):
         relation = event.relation
-        context_from_config = self._charm.model.config['nagios_context']
+        conf = self._charm.model.config
+        context_from_config = conf.get('nagios_context')
         unit = self.model.unit.name.replace('/', '-')
 
         context = ""
@@ -64,47 +67,13 @@ class Nrpe(Object):
 
         self._stored.nagios_host_context = context
 
-        hostname = ""
+        host_name = ""
         nagios_hostname = relation.__dict__.get('nagios_hostname')
 
         if nagios_hostname:
-            hostname = nagios_hostname
+            host_name = nagios_hostname
         else:
-            hostname = f"{context}-{unit}"
-
-        self._stored.nagios_hostname = hostname
-        self.on.nrpe_external_master_available.emit()
-
-    def _on_relation_broken(self, event):
-        files = self._nagios_files
-        for f in files:
-            try:
-                Path(f).unlink()
-            except Exception as e:
-                logger.debug(f"failed to remove {f}: {e}")
-        files = set()
-
-    @property
-    def _relation(self):
-        return self.framework.model.get_relation(self._relation_name)
-
-    @property
-    def _nagios_files(self):
-        return self._stored.nagios_check_files
-
-    def add_check(self,
-                  args,
-                  name,
-                  description,
-                  context,
-                  servicegroups,
-                  unit) -> None:
-        """Add nagios check."""
-        relation = self._relation
-        unit = unit.name.replace('/', '-')
-
-        context = self._stored.nagios_host_context
-        host_name = self._stored.nagios_hostname
+            host_name = f"{context}-{unit}"
 
         check_tmpl = """
 #---------------------------------------------------
@@ -124,24 +93,66 @@ define service {
     servicegroups                   %(servicegroups)s
 }
 """
-        check_filename = Path(f"/etc/nagios/nrpe.d/check_{name}.cfg")
-        check_filename.write_text(check_tmpl % {
-            'check_args': ' '.join(args),
-            'check_name': name,
-        })
-        self._nagios_files.add(str(check_filename))
+        slurm_component = self._charm.get_slurm_component()
+        slurm_process_check_args = [
+            '/usr/lib/nagios/plugins/check_procs',
+            '-c', '1:', '-C', slurm_component,
+        ]
 
-        service_filename = \
-            Path(f"/var/lib/nagios/export/service__{unit}_{name}.cfg")
-        service_filename.write_text(service_tmpl % {
-            'servicegroups': servicegroups or context,
-            'context': context,
-            'description': description,
-            'check_name': name,
-            'host_name': host_name,
-            'unit_name': unit,
-        })
-        self._nagios_files.add(str(service_filename))
+        munged_process_check_args = [
+            '/usr/lib/nagios/plugins/check_procs',
+            '-c', '1:', '-C', "munged",
+        ]
+
+        slurm_checks = [
+            {
+                'check_args': slurm_process_check_args,
+                'check_name': slurm_component,
+                'host_name': host_name,
+                'unit_name': unit,
+                'description': f"Check for {slurm_component} process.",
+                'context': conf.get('nagios_context') or nagios_host_context,
+                'servicegroups': conf.get('nagios_servicegroups') or context,
+            },
+            {
+                'check_args': munged_process_check_args,
+                'check_name': "munged",
+                'host_name': host_name,
+                'unit_name': unit,
+                'description': "Check for munged process.",
+                'context': conf.get('nagios_context') or nagios_host_context,
+                'servicegroups': conf.get('nagios_servicegroups') or context,
+            },
+        ]
+
+        for check in slurm_checks:
+            check_filename = \
+                Path(f"/etc/nagios/nrpe.d/check_{check['check_name']}.cfg")
+            check_filename.write_text(check_tmpl % check)
+
+            service_filename = \
+                Path("/var/lib/nagios/export/service__"
+                     f"{check['unit_name']}_{check['check_name']}.cfg")
+            service_filename.write_text(service_tmpl % check)
 
         iso_time = datetime.now().isoformat()
-        relation.data[self.model.unit]['timestamp'] = iso_time
+        for rel_id in _relation_ids(self._relation_name):
+            relation_cmd_line = [
+                'relation-set', '-r', rel_id, f'timestamp={iso_time}']
+            subprocess.call(relation_cmd_line)
+
+    def _on_relation_broken(self, event):
+        files = self._nagios_files
+        for f in files:
+            try:
+                Path(f).unlink()
+            except Exception as e:
+                logger.debug(f"failed to remove {f}: {e}")
+        files = set()
+
+
+def _relation_ids(reltype):
+    """List of relation_ids."""
+    relid_cmd_line = ['relation-ids', '--format=json', reltype]
+    return json.loads(
+        subprocess.check_output(relid_cmd_line).decode('UTF-8')) or []
